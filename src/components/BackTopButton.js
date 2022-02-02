9df169c008652557feb5a56ddae5386b22512f19
Enciusan <92076681+Enciusan@users.noreply.github.com>
2022-02-02 13:52:46 +0200

    Add files via upload

--- src/components/BackTopButton.js
+++ src/components/BackTopButton.js
@@ -4,10 +4,10 @@ export default function BackTopButton() {
   return (
     <a href="#nav">     
       <div className="flex relative ">
-        <div className="fixed right-10 bottom-64 ">
-          <div className="absolute w-10 h-60 rounded border-2  ">
+        <div className="fixed sm:right-10 right-7 bottom-64 ">
+          <div className="absolute sm:w-10 w-7 h-60 rounded border-2  ">
             <div className="flex-1 overflow-y-scroll">
-              <div className="absolute right-0 text-center mr-4 ">
+              <div className="absolute right-0 text-center sm:mr-4 mr-2 ">
                 <div className=" text-white text-sm font-germania">
                   <p>B</p>
                   <p>a</p>
